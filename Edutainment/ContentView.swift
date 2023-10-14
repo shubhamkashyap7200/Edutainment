54624455f163a31117518a77c64d0838f86aacaf
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-10-14 22:24:25 +0530

    Initial Commit

---
+++ Edutainment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Edutainment
+//
+//  Created by Shubham on 10/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Shubham on 10/14/23.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
